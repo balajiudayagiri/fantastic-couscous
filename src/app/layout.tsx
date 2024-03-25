fd69c4036408e6bca7565c5ada6db6b06f99bdd3
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-03-25 21:18:12 +0530

    updated view port

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${poppins.className} dark:bg-dot-white/[0.3] bg-dot-black/[0.3] m-0  relative `}>
-        <div className="z-[-1] h-screen absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="z-[-1] h-svh absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="relative overflow-y-auto h-svh scroll-styles">
           <ThemeProvider
             attribute="class"
