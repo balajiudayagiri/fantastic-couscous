5df80f7c8d365e14777bbdec1f27b41105ebeea1
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-04-02 00:19:56 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <main className="text-accent-foreground xl:mx-36 lg:mx-16 md:mx-8 sm:mx-5 mx-4 h-[70vh] flex flex-col justify-center">
       <h1 className={"md:text-7xl sm:text-6xl text-4xl font-extrabold mb-10"}>
-        I'm <br />
+        I&apos;m <br />
         Balaji Udayagiri
       </h1>
       <p className={`text-justify text-[.9rem] leading-5`}>
-        Welcome to my digital playground! I'm a{" "}
+        Welcome to my digital playground! I&apos;m a{" "}
         <span className="dark:text-cyan-500 text-cyan-600 font-bold text-xl">
           Software Engineer
         </span>{" "}
@@ -21,11 +21,11 @@ export default function Home() {
       <p className={`text-justify text-[.9rem] leading-5`}>
         With expertise in React.js, HTML5, CSS3, MUI, Mantine UI, and more, I
         transform ideas into beautiful, functional websites and applications.
-        Let's turn your vision into reality!
+        Let&apos;s turn your vision into reality!
       </p>
       <p className={`text-justify text-[.9rem] leading-5`}>
-        I'm all about growth and creativity. Join me on this journey of endless
-        learning and innovation. Together, we'll build something amazing.
+        I&apos;m all about growth and creativity. Join me on this journey of endless
+        learning and innovation. Together, we&apos;ll build something amazing.
       </p>
       <p className="text-lg mt-6 font-semibold dark:text-cyan-500 text-cyan-600">
         #SoftwareEngineering #ProductDevelopment
