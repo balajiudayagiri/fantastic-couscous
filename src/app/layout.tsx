ff9d1d47c35cf776e7fe022b5181e8b9c6dad091
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-07-12 23:22:13 +0530

    setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@b/components/theme-provider";
-import Header from "@components/header";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -23,19 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} dark:bg-dot-white/[0.3] bg-dot-black/[0.3] m-0  relative `}>
-        <div className="z-[-1] h-svh absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="relative overflow-y-auto h-svh scroll-styles">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange>
-            <Header />
-            {children}
-          </ThemeProvider>
-        </div>
+      <body className={`${poppins.className} relative `}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange>
+          {/* <Header /> */}
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
-import { SoftwareEngineericon } from "@b/_icons";
-import ScrollAnimationWrapper from "@b/components/ui/ScrollAnimationWrapper";
-import Galary from "@broot//Galary";
+import { Button } from "@b/components/ui/button";
 
-import TilesSection from "@broot/TilesSection";
-import About from "@broot/About";
 export default function Home() {
   return (
     <div className="scroll-behavior-smooth scroll-smooth snap-y snap-mandatory snap-center overflow-y-scroll h-screen transition-transform duration-300 ease-in-out">
-      <main className="lg:snap-start text-accent-foreground xl:mx-36 lg:mx-16 md:mx-8 sm:mx-5 mx-4 sm:h-dvh h-[100vh] flex flex-col justify-center">
-        <ScrollAnimationWrapper element={"div"} animationClass="slideUp">
-          <h1 className="font-bold text-2xl text-yellow-400">I&apos;m a</h1>
-          <SoftwareEngineericon className="w-full" />
-        </ScrollAnimationWrapper>
-      </main>
-      <section className="lg:snap-start container mx-auto p-4 sm:h-dvh flex items-center justify-center">
-        <About />
-      </section>
-      <section className="lg:snap-start container mx-auto p-4 sm:h-dvh flex items-center">
-        <TilesSection />
-      </section>
-      <section className="lg:snap-start container mx-auto p-4 sm:h-dvh flex items-center w-full justify-center">
-        <Galary />
-      </section>
+      root
+      <Button>click</Button>
     </div>
   );
 }
