389f8442955c5e797345a5326fdddbf70a8ce18e
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2025-02-09 15:08:17 +0530

    updates hero section

--- src/_customComponents/_app/HeroContent.tsx
+++ src/_customComponents/_app/HeroContent.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { Linkedin } from "lucide-react";
+import { Instagram, Linkedin, Mail, Twitter } from "lucide-react";
 import { Button } from "@b/components/ui/button";
+import { TwitterXIcon } from "@b/_icons";
 
 // FadeIn animation variants
 const fadeIn = {
@@ -12,13 +13,16 @@ const fadeIn = {
 
 const socialLinks = [
   { icon: Linkedin, href: "https://linkedin.com/in/yourusername" },
+  { icon: Instagram, href: "https://linkedin.com/in/yourusername" },
+  { icon: Twitter, href: "https://linkedin.com/in/yourusername" },
+  { icon: Mail, href: "https://linkedin.com/in/yourusername" },
 ];
 
 export function HeroContent() {
   return (
-    <div className="relative  min-h-[120dvh] flex items-center justify-center overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
-      <div className="absolute inset-0 flex max-md:flex-col">
-        <div className=" h-full w-1/2 bg-black">
+    <div className="relative lg:min-h-[120dvh] min-h-screen bg-black flex lg:items-center items-end justify-center overflow-hidden py-20 sm:py-20 lg:pb-32 xl:pb-36">
+      <div className="absolute inset-0 flex max-lg:flex-col">
+        <div className="h-full max-lg:hidden lg:w-1/2 overflow-visible">
           <svg
             width="675"
             height="675"
@@ -28,8 +32,8 @@ export function HeroContent() {
             className="mandala w-[80vmin] h-[80vmin]"
             style={{
               color: "currentColor",
-              fill: "hsl(var(--primary) / 0.4)",
-              animation: "spin 150s linear infinite",
+              fill: "hsl(var(--primary) / 0.3)",
+              animation: "spin 170s linear infinite",
             }}>
             <defs>
               <path
@@ -50,47 +54,81 @@ export function HeroContent() {
             </defs>
             <text dy="70" textLength="2010">
               <textPath textLength="2010" href="#circle1">
-                &nbsp;&nbsp;&nbsp;/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>&nbsp;&nbsp;&nbsp;/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>&nbsp;&nbsp;&nbsp;/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>{" "}
-                &nbsp;&nbsp;&nbsp;/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>&nbsp;&nbsp;&nbsp;/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>&nbsp;&nbsp;&nbsp;/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>{" "}
-                &nbsp;&nbsp;&nbsp;/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
-                <tspan>/</tspan>/<tspan>/</tspan>/<tspan>/</tspan>/{" "}
+                &nbsp;&nbsp;&nbsp;/<tspan>/</tspan>
+                {"/***/"}
+                <tspan>/</tspan>/ <tspan>/</tspan>/<tspan>/</tspan>
+                {"/***/"}
+                <tspan>/</tspan>/ <tspan>/</tspan>&nbsp;&nbsp;&nbsp;/
+                <tspan>/</tspan>/ <tspan>/</tspan>/<tspan>/</tspan>/
+                <tspan>/</tspan>/ <tspan>/</tspan>/<tspan>/</tspan>
+                &nbsp;&nbsp;&nbsp;/ <tspan>/</tspan>/<tspan>/</tspan>/
+                <tspan>/</tspan>/ <tspan>/</tspan>
+                {"/***/"}
+                <tspan>/</tspan>/<tspan>/</tspan> &nbsp;&nbsp;&nbsp;/
+                <tspan>/</tspan>/<tspan>/</tspan>/ <tspan>/</tspan>/
+                <tspan>/</tspan>/<tspan>/</tspan>/ <tspan>/</tspan>
+                &nbsp;&nbsp;&nbsp;/
+                <tspan>/</tspan>/ <tspan>/</tspan>/<tspan>/</tspan>/
+                <tspan>/</tspan>/ <tspan>/</tspan>
+                {"/***/"}
                 <tspan>/</tspan>
-              </textPath>
-            </text>
-            <text dy="70" textLength="1760">
-              <textPath textLength="1760" href="#circle2">
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
-                &nbsp;&nbsp;+<tspan>+</tspan>+<tspan>+</tspan>+<tspan>+</tspan>
+                &nbsp;&nbsp;&nbsp;/ <tspan>/</tspan>/<tspan>/</tspan>/
+                <tspan>/</tspan>/ <tspan>/</tspan>/<tspan>/</tspan>/
+                <tspan>/</tspan> &nbsp;&nbsp;&nbsp;/<tspan>/</tspan>/
+                <tspan>/</tspan>/ <tspan>/</tspan>/<tspan>/</tspan>/
+                <tspan>/</tspan>/ <tspan>/</tspan>
               </textPath>
             </text>
             <text dy="70" textLength="1507">
               <textPath textLength="1507" href="#circle3">
-                &lbrace;&lbrace;<tspan>&lbrace;</tspan>&lbrace;&nbsp;
-                <tspan>&rbrace;</tspan>&rbrace;&nbsp;&nbsp;&lbrace;&lbrace;
-                <tspan>&lbrace;</tspan>&lbrace;&nbsp;<tspan>&rbrace;</tspan>
-                &rbrace;&nbsp;&nbsp;&lbrace;&lbrace;<tspan>&lbrace;</tspan>
-                &lbrace;&nbsp;<tspan>&rbrace;</tspan>
-                &rbrace;&nbsp;&nbsp;&lbrace;&lbrace;<tspan>&lbrace;</tspan>
-                &lbrace;&nbsp;<tspan>&rbrace;</tspan>
-                &rbrace;&nbsp;&nbsp;&lbrace;&lbrace;<tspan>&lbrace;</tspan>
-                &lbrace;&nbsp;<tspan>&rbrace;</tspan>&rbrace;&nbsp;&nbsp;
+                &nbsp;=&gt;<tspan>+=</tspan>-=<tspan>==</tspan>=
+                <tspan>||</tspan>&amp;&amp;<tspan>{}</tspan>[]
+                <tspan>()</tspan>$#<tspan>@/</tspan>\*
+                <tspan>%</tspan>++<tspan>--</tspan>**
+                <tspan>~/</tspan>!&lt;<tspan>&gt;</tspan>!=
+                <tspan>??</tspan>?.<tspan>...</tspan>:=
+                <tspan>=&gt;</tspan>|&gt;<tspan>&lt;|</tspan>&nbsp;=&gt;
+                <tspan>+=</tspan>-=<tspan>==</tspan>=<tspan>||</tspan>&amp;&amp;
+                <tspan>{}</tspan>[]
+                <tspan>()</tspan>$#<tspan>@/</tspan>\*
+                <tspan>%</tspan>++<tspan>--</tspan>**
+                <tspan>~/</tspan>!&lt;<tspan>&gt;</tspan>!=
+                <tspan>??</tspan>?.<tspan>...</tspan>:=
+                <tspan>=&gt;</tspan>|&gt;<tspan>&lt;|</tspan>
+              </textPath>
+            </text>
+            <text dy="70" textLength="1860">
+              <textPath textLength="1860" href="#circle2">
+                {/* HTML5&nbsp;<tspan>CSS3&nbsp;</tspan>
+                JavaScript&nbsp;<tspan>TypeScript&nbsp;</tspan>
+                React&nbsp;<tspan>Next.js&nbsp;</tspan>
+                TailwindCSS&nbsp;<tspan>SCSS&nbsp;</tspan>
+                Redux&nbsp;<tspan>Zustand&nbsp;</tspan>
+                Framer&nbsp;<tspan>GSAP&nbsp;</tspan>
+                <tspan>Cypress&nbsp;</tspan>
+                Webpack&nbsp;<tspan>Vite&nbsp;</tspan>
+                SEO&nbsp;<tspan>A11y&nbsp;</tspan>
+                PWA&nbsp;<tspan>SPA&nbsp;</tspan> Architecture&nbsp;
+                <tspan>System Design&nbsp;</tspan>
+                React&nbsp;<tspan>Next.js 14&nbsp;</tspan>
+                TypeScript&nbsp;<tspan>Module Federation&nbsp;</tspan>
+                Micro-Frontends&nbsp;<tspan>Design Systems&nbsp;</tspan>
+                Server Components&nbsp;<tspan>Client Components&nbsp;</tspan>
+                Performance&nbsp;<tspan>Web Vitals&nbsp;</tspan>
+                State Machines&nbsp;<tspan>Atomic Design&nbsp;</tspan>
+                CI/CD&nbsp;<tspan>Web Workers&nbsp;</tspan>
+                Edge Runtime&nbsp;<tspan>Streaming SSR&nbsp;</tspan>
+                RSC&nbsp;<tspan>Islands Architecture&nbsp;</tspan> */}
+                &nbsp;Next.js 14&nbsp;<tspan>React 18&nbsp;</tspan>
+                TypeScript&nbsp;<tspan>Turborepo&nbsp;</tspan>
+                TailwindCSS&nbsp;<tspan>Shadcn/ui&nbsp;</tspan>
+                Tanstack Query&nbsp;<tspan>Zustand&nbsp;</tspan>
+                Framer Motion&nbsp;<tspan>GSAP&nbsp;</tspan>
+                System Design&nbsp;<tspan>Vitest&nbsp;</tspan>
+                Vite&nbsp;<tspan>SWC&nbsp;</tspan>
+                Storybook&nbsp;<tspan>Radix UI&nbsp;</tspan>
+                tRPC&nbsp;<tspan>Zod&nbsp;</tspan>
+                ESLint&nbsp;<tspan>Prettier&nbsp;</tspan>
               </textPath>
             </text>
             <text dy="70" textLength="1257">
@@ -115,41 +153,38 @@ export function HeroContent() {
             </text>
           </svg>
         </div>
-        <div className=" h-full w-1/2 bg-[url('/images/myphoto.jpg')] bg-cover bg-center bg-no-repeat"></div>
+        <div className="relative sm:h-full h-2/3 lg:w-1/2 bg-[url('/images/myphoto.jpg')] bg-cover bg-center bg-no-repeat bg-transparent">
+          <div className="sm:hidden absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-background to-transparent z-20" />
+        </div>
       </div>
       <motion.div
-        className="relative z-20 mx-auto w-4/5 px-6 lg:px-8"
+        className="relative z-20 mx-auto lg:w-4/5 w-full max-lg:h-full px-6 lg:px-8"
         initial="initial"
         animate="animate"
         variants={{
           animate: { transition: { staggerChildren: 0.1 } },
         }}>
-        <div className="text-center lg:text-left lg:mx-0">
+        <div className="md:text-center lg:text-left lg:mx-0">
           <motion.div variants={fadeIn} className="relative group">
             <span
-              className="font-extrabold text-5xl sm:text-6xl md:text-7xl xl:text-8xl 
-              bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary/80
+              className="font-extrabold text-2xl sm:text-3xl md:text-4xl xl:text-6xl 
               transition-all duration-500">
-              Hi
+              <span className="">Hi </span>
+              <span className="">I&apos;m</span>
+              <h1 className="">
+                Balaji <br className="hidden" /> Udayagiri
+              </h1>
             </span>
-            <h1
-              className="font-extrabold text-5xl sm:text-6xl md:text-7xl xl:text-8xl 
-              bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary/80
-              transition-all duration-500">
-              Balaji <br /> Udayagiri
-            </h1>
-            <div className="absolute -inset-x-6 -inset-y-4 z-[-1] bg-gradient-to-r from-primary/10 to-transparent blur-2xl" />
-          </motion.div>
+            <span className="text-white/80">
+              <p className="mt-2 sm:mt-6 text-sm sm:text-md md:text-xl  font-medium">
+                Front-End Lead Engineer
+              </p>
 
-          <motion.div variants={fadeIn} className="space-y-6">
-            <p className="mt-6 text-xl sm:text-2xl md:text-3xl  font-medium">
-              Front-End Lead Engineer
-            </p>
-
-            <p className="text-lg sm:text-xl md:text-2xl max-w-xl mx-auto lg:mx-0">
-              Transforming Figma designs into production-ready code with
-              AI-powered solutions
-            </p>
+              <p className="mt-2 text-sm sm:text-md md:text-xl max-w-xl md:mx-auto lg:mx-0">
+                Transforming Figma designs into production-ready code with
+                AI-powered solutions
+              </p>
+            </span>
           </motion.div>
 
           {/* <motion.div
@@ -172,7 +207,7 @@ export function HeroContent() {
 
           <motion.div
             variants={fadeIn}
-            className="mt-8 flex gap-6 justify-center lg:justify-start">
+            className="mt-8 flex gap-6 justify-start md:justify-center lg:justify-start text-white">
             {socialLinks.map((social, index) => (
               <motion.a
                 key={index}
