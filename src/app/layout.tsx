a25400805922b351c35ae4895053fec0f21c6d19
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2025-02-12 16:42:52 +0530

    updated meta info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -165,9 +165,7 @@ export default function RootLayout({
           name="google-site-verification"
           content="IufNjx1CiqN11ZuMQiWU7aitWu_JW5SZ7UN5AY5z9eg"
         />
-        <title>
-          Balaji Udayagiri - Front-End Lead Engineer & AI Integration Expert
-        </title>
+        <title>Balaji Udayagiri</title>
         <meta
           name="description"
           content="Front-End Lead Engineer at PureCode Software, pioneering AI-powered web solutions with expertise in React, Next.js, and TypeScript."
