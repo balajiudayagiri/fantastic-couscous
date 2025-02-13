4647e77ce964200fb8e530093b6ea496f93625c5
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2025-02-14 02:48:03 +0530

    Integrated Google Tag Manager for website analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import { FooterSection } from "@b/_customComponents/_app/FooterSection";
 import Header from "@b/_customComponents/_app/Header";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -183,8 +184,29 @@ export default function RootLayout({
             }),
           }}
         />
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T242VQ9F');`,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body className={`${poppins.className} relative `}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T242VQ9F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
