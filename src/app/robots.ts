58d40a63c0007025a069012dd25b3f37488a4f8a
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2025-04-01 01:05:22 +0530

    updated sitemap fixes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,14 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/private/",
+      },
+    ],
     sitemap: "https://balajiudayagiri.vercel.app/sitemap.xml",
+    host: "https://balajiudayagiri.vercel.app",
   };
 }
